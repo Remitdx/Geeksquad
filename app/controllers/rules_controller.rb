27476382738bef4af463adca92c9774bcab21b3f
remi <remi.tondeux@gmail.com>
2022-01-27 13:48:00 +0100

    rules CRUD 90%

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,5 +1,37 @@
 class RulesController < ApplicationController
   def index
     @rules = Rule.all
+    @rule = Rule.new #empty shell to generate simpleform new
+  end
+
+  def create
+    @rule = Rule.new(params_rule)
+    if @rule.save
+      redirect_to rules_path, notice: "Event créé!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @rule = Rule.find(params[:id])
+    @rule.update(params_rule)
+    if @rule.save
+      redirect_to rules_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @rule = Rule.find(params[:id])
+    @rule.destroy
+    redirect_to rules_path
+  end
+
+   private
+
+  def params_rule
+    params.require(:rule).permit(:title, :points, :description)
   end
 end
